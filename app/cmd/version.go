6202a4ee3a3b495559891c264bcef056cb1bfb02
fuxiaohei <fuxiaohei@vip.qq.com>
2017-03-03 10:29:55 +0800

    move some template to embed/

--- app/cmd/version.go
+++ app/cmd/version.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"runtime"
+	"time"
 
 	"github.com/go-xiaohei/pugo/app/asset"
 	"github.com/go-xiaohei/pugo/app/helper/printer"
@@ -15,7 +16,7 @@ var Version = cli.Command{
 	Usage: "print version information",
 	Action: func(cliCtx *cli.Context) error {
 		printer.Info("PuGo version \t : %v", vars.Version)
-		printer.Info("Release Date \t : %v", vars.VersionDate)
+		printer.Info("Release Date \t : %v", time.Now().Format("2006-01-02 15:04"))
 		printer.Info("Golang version \t : %v", runtime.Version())
 		printer.Info("Assets version \t : %v", asset.Date)
 		return nil
